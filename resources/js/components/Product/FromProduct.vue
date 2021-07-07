917dc4e6d2fa550d54cb5d6e8ee198d5162ec1fc
anhv <havanan95@gmail.com>
2021-07-08 01:39:00 +0700

    sửa form

--- resources/js/components/Product/FromProduct.vue
+++ resources/js/components/Product/FromProduct.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="row">
-    <div class="col-lg-6">
+    <div class="col-lg-12 mb-3">
       <div class="border border-3 p-4 rounded">
+        <h3>Chi tiết sản phẩm</h3>
         <div class="row mb-3">
           <div class="col-md-4 col-sm-12">
               <label class="form-label">Ảnh sản phẩm</label>
@@ -20,91 +21,111 @@
               <div class="row">
                 <div class="col-md-3 col-sm-12 mb-1 text-right" v-for="item in previewImages">
                   <img :src="'/cdn/products/small/'+item.name" style="width: 100%">
-                  <i class="text-danger" @click="removeImage(item)" style="cursor: pointer">x</i>
+                  <p><span>{{item.name}} </span>   <i class="text-danger" @click="removeImage(item)" style="cursor: pointer">x</i></p>
                 </div>
               </div>
           </div>
         </div>
         <div class="mb-3">
           <label for="inputProductTitle" class="form-label">Tên sản phẩm</label>
-          <input type="email" class="form-control" id="inputProductTitle" placeholder="Nhập tên sản phẩm">
+          <input type="text" class="form-control" id="inputProductTitle" placeholder="Nhập tên sản phẩm" v-model="formData.name" @change="makeSlug()">
+        </div>
+        <div class="mb-3">
+          <label for="inputProductTitle" class="form-label">Seo Url</label>
+          <input type="text" class="form-control" v-model="formData.slug">
         </div>
         <div class="mb-3">
           <div class="row">
             <div class="col-md-6 col-sm-12">
-              <label for="inputCostPerPrice" class="form-label">Loại sản phẩm</label>
-              <v-select :options="configs.categories" v-model="formData.category_id"></v-select>
+              <div class="row">
+                <div class="col-md-6 col-sm-12">
+                  <label  class="form-label">Loại sản phẩm</label>
+                  <v-select :options="configs.categories" v-model="formData.category_id"></v-select>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label  class="form-label">Đơn vị</label>
+                  <v-select :options="configs.units" v-model="formData.unit"></v-select>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label  class="form-label">Giá nhập</label>
+                  <input type="number" class="form-control"  v-model="formData.price_import">
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label  class="form-label">Giá bán</label>
+                  <input type="number" class="form-control"  v-model="formData.price">
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label  class="form-label">Giá khuyến mại</label>
+                  <input type="number" class="form-control"  v-model="formData.price_disount">
+                </div>
+                <div class="col-md-6 col-sm-12">
+                  <label for="inputStarPoints" class="form-label">Tổng số lượng</label>
+                  <input type="number" class="form-control" id="inputStarPoints" v-model="formData.total" readonly>
+                </div>
+              </div>
             </div>
             <div class="col-md-6 col-sm-12">
-              <label  class="form-label">Đơn vị</label>
-              <v-select :options="configs.units" v-model="formData.unit"></v-select>
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <label for="inputPrice" class="form-label">Giá nhập</label>
-              <input type="number" class="form-control" id="inputPrice" v-model="formData.price_import">
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <label for="inputCompareatprice" class="form-label">Giá bán</label>
-              <input type="number" class="form-control" id="inputCompareatprice" v-model="formData.price">
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <label for="inputCostPerPrice" class="form-label">Giá khuyến mại</label>
-              <input type="number" class="form-control" id="inputCostPerPrice" v-model="formData.price_disount">
-            </div>
-            <div class="col-md-6 col-sm-12">
-              <label for="inputStarPoints" class="form-label">Tổng số lượng</label>
-              <input type="number" class="form-control" id="inputStarPoints" v-model="formData.total" readonly>
+              <label for="inputProductDescription" class="form-label">Mô tả sản phẩm</label>
+              <vue-editor v-model="formData.description"></vue-editor>
             </div>
           </div>
-        </div>
-        <div class="mb-3">
-          <label for="inputProductDescription" class="form-label">Mô tả sản phẩm</label>
-          <vue-editor v-model="formData.description"></vue-editor>
         </div>
       </div>
     </div>
-    <div class="col-lg-6">
+    <div class="col-lg-12">
       <div class="border border-3 p-4 rounded">
-        <div class="row mb-3" v-for="(detail,key) in formData.details">
-          <div class="col-md-4 col-sm-12">
-            <label  class="form-label">Trạng thái</label>
-            <v-select :options="configs.status" v-model="formData.details[key].status"></v-select>
-          </div>
-          <div class="col-md-4 col-sm-12">
-            <label class="form-label">Màu sắc</label>
-            <v-select :options="configs.colors" v-model="formData.details[key].color"></v-select>
-          </div>
-          <div class="col-md-4 col-sm-12">
-            <label class="form-label">Size</label>
-            <v-select :options="configs.sizes" v-model="formData.details[key].size"></v-select>
-          </div>
-          <div class="col-md-4 col-sm-12">
-            <label for="inputPrice" class="form-label">Giá nhập</label>
-            <input type="number" class="form-control" id="inputPrice" v-model="formData.price_import">
-          </div>
-          <div class="col-md-4 col-sm-12">
-            <label for="inputCompareatprice" class="form-label">Giá bán</label>
-            <input type="number" class="form-control" id="inputCompareatprice" v-model="formData.price">
-          </div>
-          <div class="col-md-4 col-sm-12">
-            <label for="inputCostPerPrice" class="form-label">Giá khuyến mại</label>
-            <input type="number" class="form-control" id="inputCostPerPrice" v-model="formData.price_disount">
-          </div>
-          <div class="col-12 text-right mt-3">
-            <button class="btn btn-danger" @click="removeDetail(detail)"> <b-icon icon="trash-fill" aria-hidden="true"></b-icon></button>
-          </div>
-
-        </div>
-        <div class="row mb-3">
-          <div class="col-12 text-right">
-            <button type="button" class="btn btn-primary" @click="addDetail()"><b-icon icon="file-plus" aria-hidden="true"></b-icon></button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="d-grid">
-              <button type="button" class="btn btn-primary">Lưu</button>
+        <h3>Thuộc tính sản phẩm</h3>
+        <template v-for="(detail,key) in formData.details">
+          <div class="row mb-3">
+            <div class="col-md-3 col-sm-12">
+              <label  class="form-label">Ảnh</label>
+              <v-select :options="previewImages" v-model="formData.details[key].image">
+                <template slot="option" slot-scope="option">
+                  <img :src="'/cdn/products/small/'+option.name" style="width: 50px" />
+                  {{ option.name }}
+                </template>
+              </v-select>
             </div>
+            <div class="col-md-3 col-sm-12">
+              <label  class="form-label">Trạng thái</label>
+              <v-select :options="configs.status" v-model="formData.details[key].status"></v-select>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <label class="form-label">Màu sắc</label>
+              <v-select :options="configs.colors" v-model="formData.details[key].color"></v-select>
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <label class="form-label">Size</label>
+              <v-select :options="configs.sizes" v-model="formData.details[key].size"></v-select>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3 col-sm-12">
+              <label class="form-label">Số lượng</label>
+              <input type="number" min="0" v-model="formData.details[key].total" class="form-control" @change="updateTotal()">
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <label  class="form-label">Giá nhập</label>
+              <input type="number" class="form-control"  v-model="formData.details[key].price_import">
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <label  class="form-label">Giá bán</label>
+              <input type="number" class="form-control"  v-model="formData.details[key].price">
+            </div>
+            <div class="col-md-3 col-sm-12">
+              <label  class="form-label">Giá khuyến mại</label>
+              <input type="number" class="form-control"  v-model="formData.details[key].price_disount">
+            </div>
+            <div class="col-12 text-right mt-3">
+              <button class="btn btn-danger" @click="removeDetail(detail)"> <b-icon icon="trash-fill" aria-hidden="true"></b-icon></button>
+            </div>
+
+          </div>
+        </template>
+        <div class="row mt-3 mb-3">
+          <div class="col-12 text-right">
+            <button type="button" class="btn btn-success mr-2" @click="addDetail()"><b-icon icon="file-plus" aria-hidden="true"></b-icon></button>
+            <button type="button" class="btn btn-primary" @click="pushData()">Lưu</button>
           </div>
         </div>
       </div>
@@ -142,6 +163,7 @@
       },
       formData:{
         name:'',
+        slug:'',
         code:'',
         price_import:0,
         price:0,
@@ -155,7 +177,7 @@
         category_id:null,
         color:0,
         size:0,
-        details:[{status:null,color:null,size:null}]
+        details:[{status:null,color:null,size:null,price_import:null,price_discount:null,price:null,total:0,image:null}]
       },
       previewImages:[],
     }
@@ -210,13 +232,63 @@
       if(index > -1){
         productDetails.splice(index, 1);
         vm.formData.details =  productDetails;
+        vm.updateTotal()
       }
     },
     addDetail(){
       const vm = this
       const productDetails = vm.formData.details;
-      productDetails.push({status:null,color:null,size:null})
-    }
+      productDetails.push({status:null,color:null,size:null,price_import:null,price_discount:null,price:null,total:0,image:null})
+    },
+    makeSlug(){
+      const vm = this
+      let slug = ''
+      const name = vm.formData.name
+      if (name) {
+        //Đổi chữ hoa thành chữ thường
+        slug = name.toLowerCase();
+
+        //Đổi ký tự có dấu thành không dấu
+        slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+        slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+        slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+        slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+        slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+        slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+        slug = slug.replace(/đ/gi, 'd');
+        //Xóa các ký tự đặt biệt
+        slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '');
+        //Đổi khoảng trắng thành ký tự gạch ngang
+        slug = slug.replace(/ /gi, " - ");
+        //Đổi nhiều ký tự gạch ngang liên tiếp thành 1 ký tự gạch ngang
+        //Phòng trường hợp người nhập vào quá nhiều ký tự trắng
+        slug = slug.replace(/\-\-\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-\-/gi, '-');
+        slug = slug.replace(/\-\-/gi, '-');
+        //Xóa các ký tự gạch ngang ở đầu và cuối
+        slug = '@' + slug + '@';
+        slug = slug.replace(/\@\-|\-\@|\@/gi, '');
+      }
+      vm.formData.slug = slug
+    },
+    pushData(){
+      const vm = this
+      console.log(vm.formData)
+    },
+    updateTotal(){
+      const vm = this
+      const details = vm.formData.details
+      let total = 0
+      if(details.length > 0 ) {
+        for (let i = 0; i < details.length;i++ ) {
+          if(details[i].total) {
+            total += parseInt(details[i].total)
+          }
+        }
+      }
+      vm.formData.total = total
+    },
   }
 }
 </script>
